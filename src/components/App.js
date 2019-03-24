0e704ae775ea2ffbe8d6415061f97e015f7c5636
Haur Kang <haur.kang@gmail.com>
2019-03-24 22:18:21 +0000

    imported searchbar into app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import SearchBar from './SearchBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div >
+        <SearchBar />
       </div>
     );
   }
