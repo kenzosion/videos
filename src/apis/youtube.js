41435e0b450f1763dfa127f7562b24a8feb484ea
Haur Kang <haur.kang@gmail.com>
2019-03-26 22:44:34 +0000

    installed axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,14 @@
+import axios from 'axios';
+
+
 const KEY = "AIzaSyDAstTm12AtobH8_FS14B_29NZycvBW7lw";
+
+export default axios.create({
+    baseURL:  'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
+
